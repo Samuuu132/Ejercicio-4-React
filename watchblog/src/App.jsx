67516e5c379cdc9de7c99e7b729d7cc2f305e911
Samuuu132 <gud24451@ug.edu.gt>
2026-04-29 09:13:45 -0600

    Mínimo 3 rutas: home /, listado /items, detalle /items/:id (viendo que las rutas si esten funcionando)

--- watchblog/src/App.jsx
+++ watchblog/src/App.jsx
@@ -1,9 +1,12 @@
+import { Routes, Route } from 'react-router-dom'
+
 function App() {
   return (
-    <div>
-      <h1>Relojes Blog</h1>
-
-    </div>
+    <Routes>
+      <Route path="/" element={<h1>Home</h1>} />
+      <Route path="/items" element={<h1>Listado de Relojes</h1>} />
+      <Route path="/items/:id" element={<h1>Detalle del Reloj</h1>} />
+    </Routes>
   )
 }
 
